192dcea2f1ddf5e49cb145aca12510ebafe118ac
Mykola Konyk <mykola@konyk.org>
2014-11-28 01:12:23 -0500

    Adding solution to RNA problem.

---
+++ rosalind_rna.erl
@@ -0,0 +1,14 @@
+% Rosalind, problem RNA.
+% 11/28/2014.
+
+% GATGGAACTTGACTACGTAAATT
+% GAUGGAACUUGACUACGUAAAUU
+
+-module(rosalind_rna).
+-export([start/0]).
+
+replace_ut([84 | T]) -> "U" ++ replace_ut(T);
+replace_ut([H | T]) -> [H] ++ replace_ut(T);
+replace_ut([]) -> [].
+
+start() -> io:format("~p", [replace_ut("GATGGAACTTGACTACGTAAATT")]).
