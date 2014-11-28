2114b075f4d914f96133754fbbd8a0b77aca0b0d
Mykola Konyk <mykola@konyk.org>
2014-11-28 01:33:19 -0500

    Adding solution to FIB problem.

---
+++ rosalind_fib.erl
@@ -0,0 +1,14 @@
+% Rosalind, problem REVC.
+% 11/28/2014.
+
+% 5 3
+% 19
+
+-module(rosalind_fib).
+-export([start/0]).
+
+fib(1, _) -> 1;
+fib(2, _) -> 1;
+fib(X, Gen) -> fib(X - 1, Gen) + Gen * fib(X - 2, Gen).
+
+start() -> io:format("~w", [fib(5, 3)]).
