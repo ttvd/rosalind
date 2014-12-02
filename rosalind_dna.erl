17a5f1268061e068ac9ee0d76b26e8d4cdbb914d
Mykola Konyk <mykola@konyk.org>
2014-12-01 23:02:25 -0500

    Updating DNA problem to use common module.

--- rosalind_dna.erl
+++ rosalind_dna.erl
@@ -4,15 +4,20 @@
 % AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC
 % A C G T
 
-% 65 - A, 67 - C, 71 - G, 84 - T
-
 -module(rosalind_dna).
 -export([start/0]).
+-import(rosalind, [nucleobase_list/1, nucleobase_list_print/1]).
 
-count_agct([65 | T], [Ac, Cc, Gc, Tc]) -> count_agct(T, [Ac + 1, Cc, Gc, Tc]);
-count_agct([67 | T], [Ac, Cc, Gc, Tc]) -> count_agct(T, [Ac, Cc + 1, Gc, Tc]);
-count_agct([71 | T], [Ac, Cc, Gc, Tc]) -> count_agct(T, [Ac, Cc, Gc + 1, Tc]);
-count_agct([84 | T], [Ac, Cc, Gc, Tc]) -> count_agct(T, [Ac, Cc, Gc, Tc + 1]);
-count_agct([], C) -> C.
+count_nucleobases(L) -> count_nucleobases(L, [0, 0, 0, 0]).
+count_nucleobases([], C) -> C;
+count_nucleobases([H | T], [Ac, Cc, Gc, Tc]) ->
+    case H of
+        adenine -> count_nucleobases(T, [Ac + 1, Cc, Gc, Tc]);
+        cytosine -> count_nucleobases(T, [Ac, Cc + 1, Gc, Tc]);
+        guanine -> count_nucleobases(T, [Ac, Cc, Gc + 1, Tc]);
+        thymine -> count_nucleobases(T, [Ac, Cc, Gc, Tc + 1])
+    end.
 
-start() -> io:format("~w", [count_agct("AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC", [0, 0, 0, 0])]).
+start() ->
+    L = nucleobase_list("AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"),
+    io:format("~w", [count_nucleobases(L)]).
