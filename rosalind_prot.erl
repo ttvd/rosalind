a0a79cdc06c694e4a3af7bb5f0a5a95cce545e7f
Mykola Konyk <mykola@konyk.org>
2014-12-07 23:27:31 -0500

    Updating PROT problem.

--- rosalind_prot.erl
+++ rosalind_prot.erl
@@ -4,10 +4,10 @@
 -module(rosalind_prot).
 -export([start/0]).
 -import(rosalind_util_nucleobase, [nucleobase_list/1]).
--import(rosalind_util_protein, [protein_list_print/1, protein_list/1, protein_list_string/1]).
+-import(rosalind_util_protein, [protein_list_print/1, protein_list_from_nucleobase/1, protein_list_string/1]).
 
 start() ->
     NS = "AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA",
     NL = nucleobase_list(NS),
-    PL = protein_list(NL),
+    PL = protein_list_from_nucleobase(NL),
     protein_list_print(PL).
